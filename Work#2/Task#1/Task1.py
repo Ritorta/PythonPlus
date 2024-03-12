8c573052c2b0ffb5d6c5363ee5bfb2871b0b96e6
Ritorta <meddesu@yandex.ru>
2024-03-12 18:48:03 +0300

    task 1 done

--- Work#2/Task#1/Task1.py
+++ Work#2/Task#1/Task1.py
@@ -128,6 +128,71 @@
 # Процент за снятие - 1,5% от суммы снятия, но не менее 30 и не более 600 у.е
 # После каждой третьей операции пополнения или снятия начисляются проценты - 3%
 # Нельзя снять больше, чем на счёте
-# При превышении суммы в 5 млн, вычитать налог на богаство 10% перед каждой операцией, даже ошибочной
+# При превышении суммы в 5 млн, вычитать налог на богаство 10% перед каждой операцией, 
+# даже ошибочной
 # Любое действие выводит сумму денег
 
+import decimal
+
+decimal.getcontext().prec = 10
+
+CMD_DEPOSIT = 'p'
+CMD_WITHDRAW = 'c'
+CMD_EXIT = 'e'
+RICHNESS_SUM = decimal.Decimal(5_000_000)
+RICHNESS_TAX = decimal.Decimal(10) / decimal.Decimal(100)
+WITHDRAW_PERCENT = decimal.Decimal(15) / decimal.Decimal(1000)
+ADD_PERCENT = decimal.Decimal(3) / decimal.Decimal(100)
+MULTIPLICITY = decimal.Decimal(50)
+MAX_REMOVAL = decimal.Decimal(600)
+MIN_REMOVAL = decimal.Decimal(30)
+COUNT_OPEN = decimal.Decimal(3)
+
+account_balance = decimal.Decimal(0)
+count = 0
+
+while True:
+    command = input(f'Replenish - "{CMD_DEPOSIT}", Take off - "{CMD_WITHDRAW}", Exit - "{CMD_EXIT}": ')
+
+    if command == CMD_EXIT:
+        print(f'Please, take your credit cart, your balance {account_balance} y.e, Good bye!')
+        exit(0)
+
+    if account_balance > RICHNESS_SUM:
+            percent = account_balance * RICHNESS_TAX
+            account_balance -= percent
+            print(f'Wealth tax withheld {RICHNESS_TAX}% in size {percent} y.e \n'
+                  f'Total left on the credit card {account_balance} y.e')
+            
+    if command in (CMD_DEPOSIT, CMD_WITHDRAW):
+        amount = 1
+        while amount % 50 != 0:
+            amount = int(input(f'Enter sum, equals {MULTIPLICITY}: '))
+            
+    if command == CMD_DEPOSIT:
+        account_balance += amount
+        count += 1
+        print(f'Credit card replenishment {amount},\n'
+                f'Total left on the credit card {account_balance} y.e')
+    elif command == CMD_WITHDRAW:
+        withdraw_tax = amount * WITHDRAW_PERCENT
+        withdraw_tax = (MIN_REMOVAL if withdraw_tax < MIN_REMOVAL 
+                        else MAX_REMOVAL if withdraw_tax > MAX_REMOVAL else withdraw_tax)
+        if account_balance >= amount + withdraw_tax:
+            count += 1
+            account_balance -= (amount + withdraw_tax)
+            print(f'Credit card withdrawal {amount} y.e\n'
+                  f'commission for withdrawal {withdraw_tax} y.e\n'
+                  f'There is still money left on the credit card {account_balance} y.e')
+        else:
+             print(f'Insufficient funds on the credit card\n'
+                   f'The requested amount {amount + withdraw_tax} y.e, the commission was {withdraw_tax}%\n'
+                   f'Balance credit card {account_balance} y.e')
+             
+    if count and count % COUNT_OPEN == 0:
+        bonus_percent = account_balance * ADD_PERCENT
+        account_balance += bonus_percent
+        print(f'About credited {ADD_PERCENT}% for {bonus_percent} y.e\n'
+              f'Balance credit card {account_balance} y.e')
+         
+            
