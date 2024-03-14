a358da77d8c3127dd6e886452aa7caf4036a07d3
Ritorta <Meddesu@yandex.ru>
2024-03-14 12:18:12 +0500

    done

--- Work#3/Task#4/Task4.py
+++ Work#3/Task#4/Task4.py
@@ -7,6 +7,43 @@
 # Вариант № 1 - Без задания с звёздочкой
 #-------------
 
+# camping_stuff = {'спички': 0.1, 'спальник': 2, 'топор': 1, 'косметичка': 0.5, 'нож': 0.3, 'фляга с водой': 0.8, 'еда': 5, 'походная посуда': 4}
+
+# MAX_CAPACITY = 5
+
+# backpack = {}
+# maximum_weight = 0
+
+# for item, weight in camping_stuff.items():
+#     if maximum_weight + weight <= MAX_CAPACITY:
+#         backpack[item] = weight
+#         maximum_weight += weight
+
+# print(f'Backpack stuff: {backpack}, weight = {maximum_weight}')
+
+#-------------
+# Вариант № 2
+#-------------
+
+# camping_stuff = {'спички': 0.1, 'спальник': 2, 'топор': 1, 'косметичка': 0.5, 'нож': 0.3, 'фляга с водой': 0.8, 'еда': 5, 'походная посуда': 4}
+
+# MAX_CAPACITY = 5
+
+# backpack = {}
+# maximum_weight = 0
+
+# for item, weight in camping_stuff.items():
+#     if maximum_weight + weight <= MAX_CAPACITY:
+#         backpack[item] = weight
+#         maximum_weight += weight
+#         print(f'Backpack stuff: {backpack}, weight = {maximum_weight}')
+
+#-------------
+# Вариант № 3
+#-------------
+
+import itertools
+
 camping_stuff = {'спички': 0.1, 'спальник': 2, 'топор': 1, 'косметичка': 0.5, 'нож': 0.3, 'фляга с водой': 0.8, 'еда': 5, 'походная посуда': 4}
 
 MAX_CAPACITY = 5
@@ -14,14 +51,8 @@ MAX_CAPACITY = 5
 backpack = {}
 maximum_weight = 0
 
-for item, weight in camping_stuff.items():
-    if maximum_weight + weight <= MAX_CAPACITY:
-        backpack[item] = weight
-        maximum_weight += weight
-
-print(f'Backpack stuff: {backpack}, weight = {maximum_weight}')
-
-#-------------
-# Вариант № 2
-#-------------
-
+for i in range(1, len(camping_stuff) + 1):
+    for items in itertools.combinations(camping_stuff.items(), i):
+        maximum_weight = sum(weight for item, weight in items)
+        if maximum_weight <= MAX_CAPACITY:
+            print(dict(items).keys(), f'weight = {maximum_weight}')
