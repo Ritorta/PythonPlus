454410f1dc467095f4ae1c9b2856fd51c2dad95e
Ritorta <Meddesu@yandex.ru>
2024-03-18 16:52:00 +0500

    task 2 done

--- Work#4/Task#2/Task#2.py
+++ Work#4/Task#2/Task#2.py
@@ -1,2 +1,66 @@
 # 2. Напишите функцию для транспонирования матрицы
 
+#-------------
+# Вариант № 1 - С использованием вложеного цикла
+#-------------
+
+# matrix = [[1, 10, 100], [2, 20, 200], [3, 30, 300]]
+
+# result = [[0 for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+# for i in range(len(matrix)):
+#     for j in range(len(matrix[0])):    
+#         result[j][i] = matrix[i][j]
+
+# print(matrix)
+# print(result)
+
+#-------------
+# Вариант № 2 - С использованием Numpy transpose() 
+#-------------
+
+# import numpy as np
+
+# matrix = np.array([[1, 10, 100], [2, 20, 200], [3, 30, 300]])
+
+# result = matrix.transpose()
+
+# print(matrix)
+# print(result)
+
+#-------------
+# Вариант № 3 - С использованием генератора списка
+#-------------
+
+# matrix = [[1, 10, 100], [2, 20, 200], [3, 30, 300]]
+
+# result = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+
+# print(matrix)
+# print(result)
+
+#-------------
+# Вариант № 4 - С использованием pymatrix
+#-------------
+
+# import pymatrix
+
+# matrix = pymatrix.matrix([[1, 10, 100], [2, 20, 200], [3, 30, 300]])
+
+# result = matrix.trans()
+
+# print(matrix)
+# print(result)
+
+#-------------
+# Вариант № 5 - С использованием metod zip
+#-------------
+
+matrix = [[1, 10, 100], [2, 20, 200], [3, 30, 300]]
+
+matrix_zip = zip(*matrix)
+
+result = [list(row) for row in matrix_zip]
+
+print(matrix)
+print(result)
