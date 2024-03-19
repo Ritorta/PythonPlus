9174ee9c781b52669fcce7aa88cb88d242540573
Ritorta <Meddesu@yandex.ru>
2024-03-19 11:29:29 +0500

    task 4 done

--- Work#5/Task#4/Task#4.py
+++ Work#5/Task#4/Task#4.py
@@ -1,1 +1,49 @@
 # 4. Создайте функцию генератор чисел Фибоначчи
+
+#-------------
+# Вариант № 1
+#-------------
+
+# a = 0
+# b = 1
+
+
+# def fib(a, b, num):
+#     for _ in range(num):
+#         yield a
+#         a, b = b, a + b
+
+
+# while True:
+#     try:
+#         num = int(input('Enter the number" '))
+#         print(list(fib(a, b, num)))
+#     except ValueError:
+#         print('Please, enter the number')
+#         continue 
+
+#-------------
+# Вариант № 2
+#-------------
+
+
+def fib():
+    a = 0
+    b = 1
+    while True:
+        yield a
+        a, b = b, a + b
+
+
+fib_list = fib()
+
+print(f'1. {next(fib_list)}')
+print(f'2. {next(fib_list)}')
+print(f'3. {next(fib_list)}')
+print(f'4. {next(fib_list)}')
+print(f'5. {next(fib_list)}')
+print(f'6. {next(fib_list)}')
+print(f'7. {next(fib_list)}')
+print(f'8. {next(fib_list)}')
+print(f'9. {next(fib_list)}')
+print(f'10. {next(fib_list)}')
