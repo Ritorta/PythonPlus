21f6a596cc8616dc07bc4d05e70c7ca9074f5a14
Ritorta <Meddesu@yandex.ru>
2024-03-19 10:49:10 +0500

    task3 done

--- Work#4/Task#3/Task#3.py
+++ Work#4/Task#3/Task#3.py
@@ -2,9 +2,37 @@
 # где ключ — значение переданного аргумента, а значение — имя аргумента. 
 # Если ключ не хешируем, используйте его строковое представление.
 
-def convert_to_dict():
-    
-    return 0
+#-------------
+# Вариант № 1
+#-------------
 
+# def argument_dict(**kwargs):
+#     result = {}
+#     for key, value in kwargs.items():
+#         if isinstance(value, dict):
+#             value = tuple(value.items())
+#         if isinstance(value, list):
+#             value = tuple(value)  # 
+#         if hasattr(key, '__hash__'):
+#             result[value] = key
+#         else:
+#             result[str(value)] = key
+#     return result
 
-print(convert_to_dict(a = 1, b = 'Pony', c = [1, 2, 3], d = (4, 5), e = 3.14))
+# print(argument_dict(name = 'Rita', b = 'Pony', months = ['January', 'February', 'March'], courses = {'python' : 'ver 3.12', 'c#' : 'ver 2.5'}, c = [1, 2, 3], d = (4, 5), e = 3.14))
+
+#-------------
+# Вариант № 2
+#-------------
+
+def argument_dict(**kwargs):
+    result = {}
+    for key, value in kwargs.items():
+        try:
+            hash(key)
+            result[value] = key
+        except TypeError:
+            result[str(value)] = key
+    return result
+
+print(argument_dict(name = 'Rita', b = 'Pony', months = ['January', 'February', 'March'], courses = {'python' : 'ver 3.12', 'c#' : 'ver 2.5'}, c = [1, 2, 3], d = (4, 5), e = 3.14))
