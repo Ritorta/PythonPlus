7c3ff78c2e56d24d32dbff05d1a9c8c6f695677e
Ritorta <Meddesu@yandex.ru>
2024-03-19 11:40:03 +0500

    done task 3

--- Work#5/Task#3/Task3.py
+++ Work#5/Task#3/Task3.py
@@ -4,3 +4,15 @@
 # В результате получаем словарь с именем в качестве ключа и суммой премии 
 # в качестве значения. Сумма рассчитывается как ставка умноженная на 
 # процент премии 
+
+string = ['Rita', 'Ira', "Luna"]
+integer = [15000, 20000, 25000]
+percent = ['1.45%', '5.85%', '10.25%']
+
+print({name: salary * float(award[:-1]) / 100 for name, salary, award in zip(string, integer, percent)})
+
+result_dict = {name: rate * (1 + float(bonus.strip('%')) / 100) for name, rate, bonus in zip(string, integer, percent)}
+print(result_dict)
+
+result_dict = {string[i]: integer[i] + integer[i] * float(percent[i].strip('%')) / 100 for i in range(len(string))}
+print(result_dict)
