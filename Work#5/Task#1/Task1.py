f60342a23f33be055b93f30bbf98b2544a07eb14
Ritorta <Meddesu@yandex.ru>
2024-03-21 17:27:02 +0500

    W5T1 DOne

--- Work#5/Task#1/Task1.py
+++ Work#5/Task#1/Task1.py
@@ -138,3 +138,21 @@
 # Для проверки числа на простоту используйте
 # правило: "число является простым, если
 # делиться нацело только на единицу и на себя"
+
+
+def is_prime(num: int) -> int:
+    if num % 2 == 0 and num != 2:
+        return False
+    for div in range(3, int(num ** 0.5) + 1):
+        if num % div == 0:
+            return False
+    return True 
+
+
+def prime_generator(n: int) -> int:
+    for number in range(2, n + 1):
+        if is_prime(number):
+            yield number
+
+
+print(*prime_generator(13))
