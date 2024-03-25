0ae048095d6cafe8d790e2976d339abd8d8da055
Ritorta <Meddesu@yandex.ru>
2024-03-25 15:05:36 +0500

    fix name fuc in task 3, fix fuc print_board, fix variant 2 in task 4

--- Work#6/Task#3-4/Task3.py
+++ Work#6/Task#3-4/Task3.py
@@ -34,7 +34,7 @@
 #-----------
 
 
-def cheak_attack(board: int, row: int, column: int) -> bool:
+def check_attack(board: int, row: int, column: int) -> bool:
 
     n = len(board)
 
@@ -58,16 +58,16 @@ def cheak_attack(board: int, row: int, column: int) -> bool:
     return False
 
 
-def queens(board: int, row = 0) -> bool:
+def place_queens(board: int, row = 0) -> bool:
 
     n = len(board)
 
     if row == n:
         return True, board
     for column in range(n):
-        if not cheak_attack(board, row, column):
+        if not check_attack(board, row, column):
             board[row][column] = 1
-            success, final_board = queens(board, row + 1)
+            success, final_board = place_queens(board, row + 1)
             if success:
                 return True, final_board
             board[row][column] = 0
@@ -87,25 +87,20 @@ def print_board(board: list) -> list:
     for row in board:
         print(" ".join("Q" if cell == 1 else "." for cell in row))
 
-    board = [[0] * 8 for _ in range(8)]
-
-    for row, col in queens_positions:
-        board[row - 1][col - 1] = 1
-
-    if result:
-        print(f'Successful, queens dont beat other = {result}')
-    else:
-        print(f'Fail, queens beat each other = {result}')
-
 
 if __name__ == '__main__':
 
     queens_positions = [(1, 4), (2, 2), (3, 7), (4, 3), (5, 6), (6, 8), (7, 5), (8, 1)]
 
-    board = [[0]*8 for _ in range(8)]
+    board = [[0] * 8 for _ in range(8)]
     for row, col in queens_positions:
         board[row - 1][col - 1] = 1
 
-    success, final_board = queens(board)
-    result = atack_queens([row.index(1) + 1 for row in final_board])
+    success, final_board = place_queens(board)
+    result = attack_queens([row.index(1) + 1 for row in final_board])
     print_board(board)
+
+    if result:
+        print(f'Successful, queens dont beat other = {result}')
+    else:
+        print(f'Fail, queens beat each other = {result}')
--- Work#6/Task#3-4/Task4.py
+++ Work#6/Task#3-4/Task4.py
@@ -39,47 +39,12 @@
 #-----------
 # Вариант №2
 #-----------
-        
-# from Task3 import atack_queens as aq
-# from Task3 import print_board as pb
-
-# import random
-
-
-# def random_cordinat_queens():
-    
-#     count = 0
-#     result = []
-
-#     while count < 4:
-        
-#         queens_positions = [(i + 1, random.randint(1, 8)) for i in range(8)]
-        
-#         board = [pos[1] for pos in queens_positions]
-#         cheak_board = aq(board)
-        
-#         if cheak_board:
-#             result.append(queens_positions)
-#             count += 1
-            
-#     return result
-
-
-# if __name__ == '__main__':
-#     result = random_cordinat_queens()
-    
-#     for nn, value in enumerate(result, start=1):
-#         print(f'Successful arrangement № {nn}: {value}')
-        
-#         board = [[0]*8 for _ in range(8)]
-#         for row, column in value:
-#             board[row - 1][column - 1] = 1
-#         pb(board)
 
 from Task3 import attack_queens as aq
 from Task3 import print_board as pb
 
 import random
+
 
 def random_coordinate_queens():
     
@@ -104,9 +69,9 @@ if __name__ == '__main__':
     result = random_coordinate_queens()
     
     for nn, value in enumerate(result, start=1):
+        board = [[0]*8 for _ in range(8)]
+        for row, col in value:
+            board[row - 1][col - 1] = 1
+        pb(board)
         print(f'Successful arrangement № {nn}: {value}')
         
-        board = [[0]*8 for _ in range(8)]
-        for row, column in value:
-            board[row - 1][column - 1] = 1
-        pb(board)
