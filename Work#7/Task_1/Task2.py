73c2e9073e2bf31fa3bc39da128fdfb4fe55e1fe
Ritorta <Meddesu@yandex.ru>
2024-03-26 15:56:58 +0500

    task2 done

--- Work#7/Task_1/Task2.py
+++ Work#7/Task_1/Task2.py
@@ -5,4 +5,27 @@
 # среди которых обязательно должны быть гласные.
 # Полученные имена сохраните в файл.
 
+from random import randint, choice
+from pathlib import Path
 
+VOWELS = 'eyuioa'
+CONSONANTS = 'qwrtpsdfghjkzxcvbnm'
+MIN_LEN = 4
+MAX_LEN = 7
+
+
+def random_names(filename: str | Path, count: int) -> None:
+    with open(filename, 'a', encoding='utf-8') as f:
+        for _ in range(count):
+            name = ''
+            cur_vowel = choice([-1, 1])
+            for i in range(randint(MIN_LEN, MAX_LEN)):
+                if cur_vowel < 0:
+                    name += choice(VOWELS)
+                else:
+                    name += choice(CONSONANTS)
+                cur_vowel *= -1
+            print(name.title(), file=f)
+
+if __name__ == '__main__':
+    random_names(Path('C:\\Users\\Esdesu\\Documents\\Материалы по обучению\\Обучение Ai\\PythonPlus\\Work#7\\Task_1\\names.txt'), 120)
