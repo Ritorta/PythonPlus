6421cfb4f90e1a116e637ef02704489d07bf03cd
Ritorta <Meddesu@yandex.ru>
2024-03-26 15:19:53 +0500

    Task1 done

--- Work#7/Task_1/Task1.py
+++ Work#7/Task_1/Task1.py
@@ -5,3 +5,20 @@
 # Первое число int, второе float разделены вертикальной чертой.
 # Минимальное число -1000, максимальное число +1000.
 # Количество строк и имя файла передаются как аргументы функции.
+
+
+from random import randint, uniform
+from pathlib import Path
+
+MIN_NUMBER = -1000
+MAX_NUMBER = 1000
+
+def fill_num(filename: str | Path, count: int) -> None:
+    with open(filename, 'a', encoding='utf-8') as f:
+        for _ in range(count):
+            num_int = randint(MIN_NUMBER, MAX_NUMBER)
+            num_float = uniform(MIN_NUMBER, MAX_NUMBER)
+            f.write(f'{num_int}|{num_float}\n')
+
+if __name__ == '__main__':
+    fill_num(Path('C:\\Users\\Esdesu\\Documents\\Материалы по обучению\\Обучение Ai\\PythonPlus\\Work#7\\Task_1\\numbers.txt'), 256)
