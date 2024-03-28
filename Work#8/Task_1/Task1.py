b13f55410ad96560fd96c6c93fd09236b2f66ff8
Ritorta <Meddesu@yandex.ru>
2024-03-28 16:41:53 +0500

    task1 done

--- Work#8/Task_1/Task1.py
+++ Work#8/Task_1/Task1.py
@@ -7,3 +7,24 @@
 # Имена пишите с большой буквы.
 # Каждую пару сохраняйте с новой строки.
 
+import json
+from pathlib import Path
+
+path = Path('C:\\Users\\Esdesu\\Documents\\Материалы по обучению\\Обучение Ai\\PythonPlus\\Work#8\\Task_1\\Result')
+
+
+def convert(file: Path) -> None:
+    my_dict = {}
+
+    with open(file, 'r', encoding='utf-8') as f:
+        for line in f:
+            name, number = line.split()
+            my_dict[name.title()] = float(number)
+
+    with open(path / f'{file.stem}.json', 'w', encoding='utf-8') as f_write:
+            json.dump(my_dict, f_write, indent=2, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    convert(Path('C:\\Users\\Esdesu\\Documents\\Материалы по обучению\\Обучение Ai\\PythonPlus\\Work#7\\Task_1\\Results\\sort\\text\\results.txt'))
+ 
