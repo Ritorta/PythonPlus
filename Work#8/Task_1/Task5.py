de1a099fc9d48d0dd08c4d19d2670bdc03fa4ea6
Ritorta <Meddesu@yandex.ru>
2024-03-29 13:10:39 +0500

    task5 done

--- Work#8/Task_1/Task5.py
+++ Work#8/Task_1/Task5.py
@@ -3,3 +3,21 @@
 # Напишите функцию, которая ищет json файлы в указанной директории
 # и сохраняет их содержимое в виде одноимённых pickle файлов.
 
+import pickle
+import json
+from pathlib import Path
+
+path = Path('C:\\Users\\Esdesu\\Documents\\Материалы по обучению\\Обучение Ai\\PythonPlus\\Work#8\\Task_1\\Result')
+
+
+def json_to_pickle(der_path: Path) -> None:
+    for file in der_path.iterdir():
+        if file.is_file() and file.suffix == '.json':
+            with open(path / file, 'r', encoding='utf-8') as f_read:
+                data =json.load(f_read)
+            with open(path / f'{file.stem}.pickle', 'wb') as f_writebyte:
+                pickle.dump(data, f_writebyte)
+
+
+if __name__ == '__main__':
+    json_to_pickle(Path(path))
